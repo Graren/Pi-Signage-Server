5fcaa17b6e11987269f2c5d2265fb073fd9253fc
Oscar <oscar.colmenares@emqu.net>
2017-11-19 21:54:32 -0400

    Django admin models

--- srv/admin.py
+++ srv/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from srv.models import Archivo, Lista, Usuario, Grupo_Dispositivos, Dispositivo
 
-# Register your models here.
+admin.site.register(Archivo)
+admin.site.register(Lista)
+admin.site.register(Usuario)
+admin.site.register(Grupo_Dispositivos)
+admin.site.register(Dispositivo)
