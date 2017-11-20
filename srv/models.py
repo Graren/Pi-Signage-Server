94c18645ecc7e89123415bd0de1fa1eb45b5f824
Oscar <oscar.colmenares@emqu.net>
2017-11-19 20:49:15 -0400

    Stuff

--- srv/models.py
+++ srv/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=100)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=64)
+
+    class Meta:
+        db_table = "usuario"
+
+class Lista(models.Model):
+    nombre= models.CharField(max_length=40)
+
+    class Meta:
+        db_table = "lista"
+
+class Archivo(models.Model):
+    user = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    lista = models.ForeignKey(Lista)
+    nombre = models.CharField(max_length=100)
+    tipo = models.CharField(max_length=30)
+
+    class Meta:
+        db_table = "archivo"
+
+class Grupo_Dispositivos(models.Model):
+    user= models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    lista= models.ForeignKey(Lista, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "grupo_dispositivos"
+
+class Dispositivo(models.Model):
+    grupo = models.ForeignKey(Grupo_Dispositivos)
+    direccion_mac = models.CharField(max_length=25)
+
+    class Meta:
+        db_table = "dispositivo"
