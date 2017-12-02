d7ba43cc30a1eba789757f59e61c127320ad967a
Oscar <oscar.colmenares@emqu.net>
2017-12-02 14:41:29 -0400

    Sweetened resources

--- srv/resources.py
+++ srv/resources.py
@@ -1,6 +1,7 @@
-from tastypie.resources import ModelResource
+from tastypie.resources import ModelResource,ALL, ALL_WITH_RELATIONS
 from tastypie import  fields
 from tastypie.authorization import Authorization
+
 from srv.models import Usuario, Dispositivo, Grupo_Dispositivos, Lista, Archivo
 
 class UsuarioResource(ModelResource):
@@ -8,12 +9,9 @@ class UsuarioResource(ModelResource):
         queryset = Usuario.objects.all()
         resource_name = 'user'
         authorization = Authorization()
-        
-class DispositivoResource(ModelResource):
-    class Meta:
-        queryset = Dispositivo.objects.all()
-        resource_name = 'device'
-        authorization = Authorization()
+        filtering = {
+            'nombre': ['exact', ],
+        }
 
 class ListaResource(ModelResource):
     class Meta:
@@ -22,15 +20,32 @@ class ListaResource(ModelResource):
         authorization = Authorization()
 
 class ArchivoResource(ModelResource):
-    user = fields.ForeignKey(UsuarioResource, 'user')
-    list = fields.ForeignKey(ListaResource, 'lista')
+    user = fields.ForeignKey(UsuarioResource, 'user',full=True)
+    list = fields.ForeignKey(ListaResource, 'lista',full=True)
     class Meta:
         queryset = Archivo.objects.all()
         resource_name = 'file'
         authorization = Authorization()
+        filtering = {
+            'user': ALL_WITH_RELATIONS,
+            'list': ALL_WITH_RELATIONS,
+            'nombre': ['exact',],
+        }
 
 class Grupo_DispositivoResource(ModelResource):
+    user = fields.ForeignKey(UsuarioResource, 'user', full=True)
+    list = fields.ForeignKey(ListaResource, 'lista', full=True)
     class Meta:
         queryset = Grupo_Dispositivos.objects.all()
         resource_name = 'deviceGroup'
         authorization = Authorization()
+
+class DispositivoResource(ModelResource):
+    group = fields.ForeignKey(Grupo_DispositivoResource, 'grupo', full=True)
+    class Meta:
+        queryset = Dispositivo.objects.all()
+        resource_name = 'device'
+        authorization = Authorization()
+        filtering = {
+            'nombre': ['exact', ],
+        }
