cd6a7645c359abdfa6c218d72f06d887d8038c89
Graren <oscar_col15@hotmail.com>
2018-01-22 00:03:02 -0400

    se llego el PAPOOOOMAAAAAAAAAAAAAaan

--- srv/consumers.py
+++ srv/consumers.py
@@ -44,7 +44,8 @@ def ws_message(message):
                     'id': file.id,
                     'format': file.tipo,
                     'url': file.url,
-                    'time': None if file.tipo == 'mp4' else file.tiempo
+                    'time': None if file.tipo == 'mp4' else file.tiempo,
+                    'adjustment': file.ajuste
                 })
             print(playlist)
             msg = {
